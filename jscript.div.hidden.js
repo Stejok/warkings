21eb8114647e448065ad5db34140a73e72607b3a
Stejok <stejochek@mail.ru>
2017-11-05 09:30:14 +0200

    adding project files

---
+++ jscript.div.hidden.js
@@ -0,0 +1,88 @@
+function warkings() {
+	location.reload();
+	document.getElementById("warkings").hidden = false;
+	document.getElementById("rules").hidden = true;
+	document.getElementById("history").hidden = true;
+	document.getElementById("glichking").hidden = true;
+	document.getElementById("gcataclysm").hidden = true;
+	document.getElementById("gmop").hidden = true;
+	document.getElementById("gwod").hidden = true;
+	document.getElementById("glegion").hidden = true;
+}
+
+function rules() {
+	document.getElementById("warkings").hidden = true;
+	document.getElementById("rules").hidden = false;
+	document.getElementById("history").hidden = true;
+	document.getElementById("glichking").hidden = true;
+	document.getElementById("gcataclysm").hidden = true;
+	document.getElementById("gmop").hidden = true;
+	document.getElementById("gwod").hidden = true;
+	document.getElementById("glegion").hidden = true;
+}
+
+function history() {
+	document.getElementById("warkings").hidden = true;
+	document.getElementById("rules").hidden = true;
+	document.getElementById("history").hidden = false;
+	document.getElementById("glichking").hidden = true;
+	document.getElementById("gcataclysm").hidden = true;
+	document.getElementById("gmop").hidden = true;
+	document.getElementById("gwod").hidden = true;
+	document.getElementById("glegion").hidden = true;
+}
+
+function glichking() {
+	document.getElementById("warkings").hidden = true;
+	document.getElementById("rules").hidden = true;
+	document.getElementById("history").hidden = true;
+	document.getElementById("glichking").hidden = false;
+	document.getElementById("gcataclysm").hidden = true;
+	document.getElementById("gmop").hidden = true;
+	document.getElementById("gwod").hidden = true;
+	document.getElementById("glegion").hidden = true;
+}
+
+function gcataclysm() {
+	document.getElementById("warkings").hidden = true;
+	document.getElementById("rules").hidden = true;
+	document.getElementById("history").hidden = true;
+	document.getElementById("glichking").hidden = true;
+	document.getElementById("gcataclysm").hidden = false;
+	document.getElementById("gmop").hidden = true;
+	document.getElementById("gwod").hidden = true;
+	document.getElementById("glegion").hidden = true;
+}
+
+function gmop() {
+	document.getElementById("warkings").hidden = true;
+	document.getElementById("rules").hidden = true;
+	document.getElementById("history").hidden = true;
+	document.getElementById("glichking").hidden = true;
+	document.getElementById("gcataclysm").hidden = true;
+	document.getElementById("gmop").hidden = false;
+	document.getElementById("gwod").hidden = true;
+	document.getElementById("glegion").hidden = true;
+}
+
+function gwod() {
+	document.getElementById("warkings").hidden = true;
+	document.getElementById("rules").hidden = true;
+	document.getElementById("history").hidden = true;
+	document.getElementById("glichking").hidden = true;
+	document.getElementById("gcataclysm").hidden = true;
+	document.getElementById("gmop").hidden = true;
+	document.getElementById("gwod").hidden = false;
+	document.getElementById("glegion").hidden = true;
+}
+
+function glegion() {
+	document.getElementById("warkings").hidden = true;
+	document.getElementById("rules").hidden = true;
+	document.getElementById("history").hidden = true;
+	document.getElementById("glichking").hidden = true;
+	document.getElementById("gcataclysm").hidden = true;
+	document.getElementById("gmop").hidden = true;
+	document.getElementById("gwod").hidden = true;
+	document.getElementById("glegion").hidden = false;
+}
